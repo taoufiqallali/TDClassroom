9366af8fffbae806d64454b7cf54eb48658bc926
Taoufiq Allali <taoufiqallali0@gmail.com>
2025-02-01 00:31:38 +0100

    sec

--- backend/src/main/java/com/M2I/TDClassroom/config/SecurityConfiguration.java
+++ backend/src/main/java/com/M2I/TDClassroom/config/SecurityConfiguration.java
@@ -14,6 +14,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -29,11 +34,24 @@ public class SecurityConfiguration {
                     .csrf(AbstractHttpConfigurer::disable)
                     .authorizeHttpRequests(http -> {
                         http.requestMatchers("/api/auth/login").permitAll();
-                        http.requestMatchers( "/api/personnes/**", "/api/locals/**").hasRole("PERSONNE");
+                        http.requestMatchers( "/api/personnes/**", "/api/locals/**").hasRole("ADMIN");
                         http.anyRequest().authenticated();
                     })
                 .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
                     .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4201")); // Your frontend URL
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true); // This allows the browser to send cookies (session)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -7,5 +7,5 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(),DatePipe, provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(),DatePipe, provideAnimationsAsync(), provideAnimationsAsync()]
 };
